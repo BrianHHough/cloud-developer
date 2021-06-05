7de081e6e2a933c0e4c422d0921cda5edf5cb76f
BrianHHough <brianhoughmedia@gmail.com>
2021-06-05 04:09:36 -0400

    Using Postbird to access db

--- course-02/exercises/udacity-c2-basic-server/sql_example.sql
+++ course-02/exercises/udacity-c2-basic-server/sql_example.sql
@@ -1,16 +1,19 @@
+-- If this thing exists, let's destroy it ( way to re-do certain things)
 DROP VIEW IF EXISTS joined;
 DROP VIEW IF EXISTS toyotas;
 DROP TABLE IF EXISTS "public"."make";
 DROP TABLE IF EXISTS "public"."cars";
 
+-- Creating a table (i.e. same as the plus button in Postbird and entered in table name)
 CREATE TABLE "public"."cars" (
-  id SERIAL PRIMARY KEY,
+  id SERIAL PRIMARY KEY, -- we are saying we will auto-increment and use this as our primary key id
   type TEXT,
   model TEXT,
   cost INT,
   make_id INT
 );
 
+-- once table created, we can insert data into it
 INSERT INTO "public"."cars" ("type", "model", "cost", "make_id") VALUES 
 	('sedan', 'roadster', '33', '2'),
 	('sedan', 'prius', '22', '1'),
